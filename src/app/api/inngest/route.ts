ec99a5dabd8eafe18977caa55c4bde530d92f98d
Zach Gorton <zachgortonnyc@gmail.com>
2025-05-23 08:43:11 -0400

    move inngest directory

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,7 +1,7 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../server/inngest/client";
-import { addUser, deleteUser, updateUser } from "../../../server/inngest/clerkFunctions";
-import { subscriptionCreated } from "~/server/inngest/stripeFunctions";
+import { inngest } from "../../../inngest/client";
+import { addUser, deleteUser, updateUser } from "../../../inngest/clerkFunctions";
+import { subscriptionCreated } from "~/inngest/stripeFunctions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
