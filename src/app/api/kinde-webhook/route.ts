576d3e7dec5e5e48add87e694b6ef483eaa444e0
Zach Gorton <zachgortonnyc@gmail.com>
2025-05-29 17:08:42 -0400

    fix typing on decoding in kinde webhook

--- src/app/api/kinde-webhook/route.ts
+++ src/app/api/kinde-webhook/route.ts
@@ -1,45 +1,32 @@
-/* eslint-disable */
-// @ts-nocheck
-
 import { NextResponse } from "next/server";
 import jwksClient from "jwks-rsa";
 import jwt from "jsonwebtoken";
 
-// The Kinde issuer URL should already be in your `.env` file
-// from when you initially set up Kinde. This will fetch your
-// public JSON web keys file
 const client = jwksClient({
   jwksUri: `${process.env.KINDE_ISSUER_URL}/.well-known/jwks.json`,
 });
 
 export async function POST(req: Request) {
   try {
-    // Get the token from the request
     const token = await req.text();
 
-    // Decode the token
-    const { header } = jwt.decode(token, { complete: true });
+    const { header } = jwt.decode(token, { complete: true })!;
     const { kid } = header;
 
-    // Verify the token
     const key = await client.getSigningKey(kid);
     const signingKey = key.getPublicKey();
-    const event = await jwt.verify(token, signingKey);
+    const event = jwt.verify(token, signingKey);
 
-    // Handle various events
+    console.log("event", event);
+
     switch (event?.type) {
       case "user.updated":
-        // handle user updated event
-        // e.g update database with event.data
         console.log(event.data);
         break;
       case "user.created":
-        // handle user created event
-        // e.g add user to database with event.data
         console.log(event.data);
         break;
       default:
-        // other events that we don't handle
         break;
     }
 
