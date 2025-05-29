f017f5580aca4a775921c122b3bdba7fe9cc8c75
Zach Gorton <zachgortonnyc@gmail.com>
2025-05-29 13:53:59 -0400

    log incoming kinde events

--- src/server/inngest/kindeFunctions.ts
+++ src/server/inngest/kindeFunctions.ts
@@ -12,37 +12,37 @@ export const addUser = inngest.createFunction(
   { event: "kinde/user.created" },
   async ({ event }) => {
     console.log("event", event);
-    const parsed = ClerkUpdateAddSchema.safeParse(event);
+    // const parsed = ClerkUpdateAddSchema.safeParse(event);
 
-    if (!parsed.success) {
-      console.error("Invalid event payload", parsed.error);
-      throw new NonRetriableError("Invalid event payload");
-    }
+    // if (!parsed.success) {
+    //   console.error("Invalid event payload", parsed.error);
+    //   throw new NonRetriableError("Invalid event payload");
+    // }
 
-    const user = parsed.data.data;
-    const { id } = user;
-    const primaryEmail = user.email_addresses.find(
-      (e) => e.id === user.primary_email_address_id,
-    );
+    // const user = parsed.data.data;
+    // const { id } = user;
+    // const primaryEmail = user.email_addresses.find(
+    //   (e) => e.id === user.primary_email_address_id,
+    // );
 
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-    const customer = await stripe.customers.create({
-      email: primaryEmail!.email_address,
-    });
+    // const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+    // const customer = await stripe.customers.create({
+    //   email: primaryEmail!.email_address,
+    // });
 
-    const subscription = await stripe.subscriptions.create({
-      customer: customer.id,
-      items: [{ price: process.env.STRIPE_FREE_PRICE_ID! }],
-    });
+    // const subscription = await stripe.subscriptions.create({
+    //   customer: customer.id,
+    //   items: [{ price: process.env.STRIPE_FREE_PRICE_ID! }],
+    // });
 
-    const periodEnd = subscription.items.data[0]!.current_period_end;
-    const periodEndDate = new Date(periodEnd * 1_000);
+    // const periodEnd = subscription.items.data[0]!.current_period_end;
+    // const periodEndDate = new Date(periodEnd * 1_000);
 
-    await db.insert(users).values({
-      clerkId: id,
-      email: primaryEmail!.email_address,
-      stripeId: customer.id,
-      subscriptionPeriodEnd: periodEndDate,
-    });
+    // await db.insert(users).values({
+    //   clerkId: id,
+    //   email: primaryEmail!.email_address,
+    //   stripeId: customer.id,
+    //   subscriptionPeriodEnd: periodEndDate,
+    // });
   },
 );
